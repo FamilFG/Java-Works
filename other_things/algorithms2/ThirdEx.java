a7adba59bd23fff0f34cb5d0d2b44c1bf5c53a11
Famil <smartosbros@gmail.com>
2025-10-02 15:44:18 +0400

    updated

--- other_things/algorithms2/ThirdEx.java
+++ other_things/algorithms2/ThirdEx.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 public class ThirdEx {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number");
+        System.out.print("Enter the size: ");
         int n = sc.nextInt();
-        int massiv[][] = new int[n][n];
+        int[][] massiv = new int[n][n];
         int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i == j) {
-                    sum+= massiv[i][j];
-                }
-
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                massiv[i][j] = sc.nextInt();
             }
         }
-        System.out.println("The sum is " + sum);
+        for (int i = 0; i < n; i++) {
+            sum += massiv[i][i];
+        }
+
+        System.out.println("The sum: " + sum);
     }
 }
